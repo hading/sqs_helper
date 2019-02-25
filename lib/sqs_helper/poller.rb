575246fe69c8ccf31253dbd229e15efa1971cacc
Howard Ding <hding2@illinois.edu>
2019-02-25 16:03:50 -0600

    Make the poller class more complete.

--- lib/sqs_helper/poller.rb
+++ lib/sqs_helper/poller.rb
@@ -1,13 +1,18 @@
 module SqsHelper
   class Poller
 
-    attr_accessor :connector, :aws_poller, :end_polling_flag, :wait_time_seconds
+    attr_accessor :connector, :aws_poller, :end_polling_flag, :wait_time_seconds, :unparsed_messages,
+                  :logger, :queue_name, :visibility_timeout
 
     def initialize(connector, queue_name, args = {})
       self.connector = connector
       self.aws_poller = connector.create_aws_poller(queue_name)
       self.end_polling_flag = false
       self.wait_time_seconds = args[:wait_time_seconds] || 60
+      self.unparsed_messages = args[:unparsed_messages]
+      self.queue_name = queue_name
+      self.visibility_timeout = args[:visibility_timeout]
+      initialize_logger(args[:logger])
       initialize_aws_poller
     end
 
@@ -17,13 +22,30 @@ module SqsHelper
       end
     end
 
+    def initialize_logger(maybe_logger)
+      self.logger = if maybe_logger
+                      maybe_logger
+                    elsif defined?(Rails) and Rails.respond_to?(:logger)
+                      Rails.logger
+                    else
+                      nil
+                    end
+    end
+
     def stop_polling
       self.end_polling_flag = true
     end
 
     def start_polling(action_callback, args = {})
-      aws_poller.poll(wait_time_seconds: wait_time_seconds) do |message, stats|
-        action_callback.call(message.body)
+      logger.info "Starting SQS polling for #{queue_name}" if logger
+      aws_poller.poll(wait_time_seconds: wait_time_seconds, visibility_timeout: visibility_timeout) do |message, stats|
+        payload = message.body
+        begin
+          payload = JSON.parse(payload) unless self.unparsed_messages
+          action_callback.call(payload)
+        rescue Exception => e
+          logger.error "Failed to handle #{queue_name} response #{payload}: #{e}" if logger
+        end
       end
     end
 
--- test/poller_test.rb
+++ test/poller_test.rb
@@ -1,10 +1,12 @@
 require_relative 'test_helper'
+require 'logger'
 
 class PollerTest < Minitest::Test
 
   def setup
     @connector = SqsHelper::Connector.new(aws_key_id: 'key_id', aws_secret_key: 'secret_key',
                                           endpoint: 'http://localhost:9324', region: 'us-east-2')
+    @logger = Logger.new(STDOUT)
     @queue = 'sqs_helper_polling_test'
   end
 
@@ -13,8 +15,7 @@ class PollerTest < Minitest::Test
   end
 
   def test_create_and_use_poller
-    my_poller = SqsHelper::Poller.new(@connector, @queue, wait_time_seconds: 0.1)
-    poller = my_poller.aws_poller
+    my_poller = SqsHelper::Poller.new(@connector, @queue, wait_time_seconds: 0.1, unparsed_messages: true, logger: @logger)
     messages = (1..10).collect do
       "x" * (rand(100) + 1)
     end
@@ -23,12 +24,7 @@ class PollerTest < Minitest::Test
     p = Proc.new {|message| @message_lengths << message.length}
     t = Thread.new do
       my_poller.start_polling(p)
-      # poller.poll(wait_time_seconds: 0.1) do |message, stats|
-      #   p.call(message)
-      #   #@message_lengths << message.body.length
-      #end
     end
-
     sleep 1
     assert_equal messages.collect {|message| message.length}, @message_lengths
     my_poller.stop_polling
