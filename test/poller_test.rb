9ea929804e5284a8ab2786058e53b45e523b0ef9
Howard Ding <hding2@illinois.edu>
2019-02-25 16:23:34 -0600

    Add test with parsing

--- test/poller_test.rb
+++ test/poller_test.rb
@@ -31,4 +31,21 @@ class PollerTest < Minitest::Test
     t.join
   end
 
+  def test_create_and_use_poller_with_parsing
+    my_poller = SqsHelper::Poller.new(@connector, @queue, wait_time_seconds: 0.1, logger: @logger)
+    message = {key: 'value', other_key: 'other_value'}
+    @connector.send_message(@queue, message)
+    @received_message = nil
+    p = Proc.new {|message| @received_message = message}
+    t = Thread.new do
+      my_poller.start_polling(p)
+    end
+    sleep 0.4
+    assert_equal 'value', @received_message['key']
+    assert_equal 'other_value', @received_message['other_key']
+    my_poller.stop_polling
+    t.join
+  end
+
+
 end
