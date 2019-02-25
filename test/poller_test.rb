78a6821b2e814def4f5776f5ef1fdb53b7928ba9
Howard Ding <hding2@illinois.edu>
2019-02-25 15:03:55 -0600

    Make the test a little bit more testy.

--- test/poller_test.rb
+++ test/poller_test.rb
@@ -3,8 +3,8 @@ require_relative 'test_helper'
 class PollerTest < Minitest::Test
 
   def setup
-    @connector = SqsHelper::Connector.new( aws_key_id: 'key_id', aws_secret_key: 'secret_key',
-                                           endpoint: 'http://localhost:9324', region: 'us-east-2')
+    @connector = SqsHelper::Connector.new(aws_key_id: 'key_id', aws_secret_key: 'secret_key',
+                                          endpoint: 'http://localhost:9324', region: 'us-east-2')
     @queue = 'sqs_helper_polling_test'
   end
 
@@ -18,15 +18,20 @@ class PollerTest < Minitest::Test
     poller.before_request do
       throw :stop_polling if @end_polling
     end
-    message = 'joebob'
-    @message_length = nil
-    @connector.send_message(@queue, message)
-    poller.poll(wait_time_seconds: 0.1) do |message, stats|
-      @message_length = message.body.length
-      @end_polling = true
+    messages = (1..10).collect do
+      "x" * (rand(100) + 1)
+    end
+    @message_lengths = Array.new
+    messages.each {|message| @connector.send_message(@queue, message)}
+    t = Thread.new do
+      poller.poll(wait_time_seconds: 0.1) do |message, stats|
+        @message_lengths << message.body.length
+      end
     end
     sleep 0.2
-    assert_equal 6, @message_length
+    assert_equal messages.collect {|message| message.length}, @message_lengths
+    @end_polling = true
+    t.join
   end
 
 end
